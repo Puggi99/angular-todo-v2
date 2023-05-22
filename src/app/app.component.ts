7b1de996f181411444cba55b1c3e7781b2ccf4d4
Puggi99 <127298032+Puggi99@users.noreply.github.com>
2023-05-22 11:50:16 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,19 @@ import { TodoAddComponent } from './components/todo-add/todo-add.component';
 export class AppComponent {
 
   title = 'angular-todo';
-
+  isLight = true
   constructor(public dialog: MatDialog){}
 
   openModal(){
     this.dialog.open(TodoAddComponent)
   }
+
+  changeTheme(){
+    document.body.classList.toggle('dark-mode')
+    this.isLight= !this.isLight
+  }
+
+
+
 
 }
